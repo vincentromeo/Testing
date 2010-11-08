e997bd831c840b2c9d937ee4e0237f8f7fa0a232
vdalocanog <soy_vin@yahoo.com>
2010-11-08 15:22:33 +0800

    added some dwr testing folders

---
+++ dwrtesting/src/main/java/testing/HelloWorld.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class HelloWorld {
+	
+	public String sayHello(String from) {
+		return "Hello from " + from;
+	}
+
+	public String wowowee() {
+		return "<table><tr><td>hi</td><td>hi</td><td>hi</td></tr><tr><td>ho</td><td>ho</td><td>ho</td></tr></table>";
+	}
+}
